6fe0908d2c2f47fe41d5c47cd1723ac268c2f942
xakep13 <xakep-bika@mail.ru>
2017-10-08 23:52:14 +0300

    Created form

---
+++ PharmacyApp/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace PharmacyApp
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PharmacyApp/PharmacyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp
+{
+    class PharmacyContext :DbContext
+    {
+        public PharmacyContext() : base("PharmacyDb") { }
+
+        DbSet<Supplier> Supppliers { get; set; }
+        DbSet<Customer> Customers { get; set; }
+    }
+}
